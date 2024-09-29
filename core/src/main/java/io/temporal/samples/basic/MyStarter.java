d9f0870b4cf4a3d38533364645d2ac8901b13fe1
Dan Davison <dan.davison@temporal.io>
2024-09-29 17:56:22 -0400

    basic sample

---
+++ core/src/main/java/io/temporal/samples/basic/MyStarter.java
@@ -0,0 +1,37 @@
+package io.temporal.samples.basic;
+
+import io.temporal.api.enums.v1.WorkflowIdReusePolicy;
+import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowOptions;
+import io.temporal.serviceclient.WorkflowServiceStubs;
+
+public class MyStarter {
+
+  // Define the task queue name
+  static final String TASK_QUEUE = "HelloUpdateTaskQueue";
+
+  // Define the workflow unique id
+  static final String WORKFLOW_ID = "HelloUpdateWorkflow";
+
+  public static void main(String[] args) throws Exception {
+
+    WorkflowServiceStubs service = WorkflowServiceStubs.newLocalServiceStubs();
+
+    WorkflowClient client = WorkflowClient.newInstance(service);
+
+    WorkflowOptions workflowOptions =
+        WorkflowOptions.newBuilder()
+            .setTaskQueue(TASK_QUEUE)
+            .setWorkflowId(WORKFLOW_ID)
+            .setWorkflowIdReusePolicy(
+                WorkflowIdReusePolicy.WORKFLOW_ID_REUSE_POLICY_TERMINATE_IF_RUNNING)
+            .build();
+    MyWorkflow workflow = client.newWorkflowStub(MyWorkflow.class, workflowOptions);
+
+    WorkflowClient.start(workflow::run);
+
+    int result = workflow.myUpdate();
+    System.out.println(result);
+    System.exit(0);
+  }
+}
